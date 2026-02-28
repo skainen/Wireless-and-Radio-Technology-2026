cf3bc1c75eaf308a11ad2ccf9961229810bef770
Santeri Karjalainen <109695772+skainen@users.noreply.github.com>
2026-02-28 15:32:55 +0200

    Create radiation_pattern.m

---
+++ Basics of Antenna Technology/3-Dimensional radiation pattern of the omni-directional antenna/radiation_pattern.m
@@ -0,0 +1,47 @@
+clc;
+close all;
+clear all;
+
+tmin = input('The lower bound of theta in degree= ');
+tmax = input('The upper bound of theta in degree= ');
+pmin = input('The lower bound of phi in degree= ');
+pmax = input('The upper bound of phi in degree= ');
+
+tinc = 2;
+pinc = 4;
+rad  = pi/180;
+
+theta1 = (tmin:tinc:tmax);
+phi1   = (pmin:pinc:pmax);
+theta  = theta1 .* rad;
+phi    = phi1   .* rad;
+
+[THETA, PHI] = meshgrid(theta, phi);
+
+y1 = input('The field pattern: E(THETA,PHI)= ');
+v  = input('The field pattern: P(THETA,PHI)= ', 's');
+
+y     = abs(y1);
+ratio = max(max(y));
+y     = y / ratio;          % normalize to unit radius
+
+[X, Y, Z] = sph2cart(PHI, pi/2 - THETA, y);
+
+figure;
+mesh(X, Y, Z);
+colormap hsv;
+axis equal;
+xlabel('X'); ylabel('Y'); zlabel('Z');
+title('3D Radiation Pattern','Color','b', ...
+      'FontName','Helvetica','FontSize',12,'FontWeight','demi');
+
+fprintf('\n Input Parameters: \n--------------------');
+fprintf('\n Theta = %2.0f', tmin);
+fprintf(' : %2.0f', tinc);
+fprintf(' : %2.0f', tmax);
+fprintf('\n Phi   = %2.0f', pmin);
+fprintf(' : %2.0f', pinc);
+fprintf(' : %2.0f', pmax);
+fprintf('\n FIELD PATTERN : %s', v);
+fprintf('\n\n Output is shown in the figure below-----------');
+fprintf('\n');
