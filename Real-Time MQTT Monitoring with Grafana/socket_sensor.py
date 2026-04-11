d6c636c539aee12cd4046d76e3a7bf3cdbc58710
Santeri Karjalainen <109695772+skainen@users.noreply.github.com>
2026-04-11 21:15:09 +0300

    Create socket_sensor.py

---
+++ Real-Time MQTT Monitoring with Grafana/socket_sensor.py
@@ -0,0 +1,22 @@
+import socket
+import random
+import time
+
+SERVER_IP = "192.168.122.1"
+PORT = 1883
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((SERVER_IP, PORT))
+
+while True:
+
+    temperature = round(random.uniform(20,35),2)
+
+    message = f"{temperature}"
+
+    client.send(message.encode())
+
+    print("Sensor value sent:", temperature)
+
+    time.sleep(5)
